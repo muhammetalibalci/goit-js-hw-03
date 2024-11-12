53d3444347335ee7c694bcd44d7252f46d5cb9b5
muhammetalibalci <muhammetalibalci60@hotmail.com>
2024-11-12 20:01:27 +0300

    codes edited

--- js/task-4.js
+++ js/task-4.js
@@ -1,25 +1,30 @@
 function getShippingCost(country) {
+    let chinaPrice = 100;
+    let chilePrice = 250;
+    let australiaPrice = 170;
+    let jamaicaPrice = 120;
 
     switch (country) {
         case "China":
-            console.log ("Shipping to China will cost 100 credits")
+            message = `Shipping to China will cost ${100} credits`
             break;
         
         case "Chile":
-            console.log ("Shipping to Chile will cost 250 credits")
+            message = `Shipping to Chile will cost ${250} credits`
             break;
         
         case "Australia":
-            console.log ("Shipping to Australia will cost 170 credits")
+            message = `Shipping to Australia will cost ${170} credits`
             break;
         
         case "Jamaica":
-            console.log("Shipping to Jamaica will cost 120 credits")
+            message = `Shipping to Jamaica will cost ${120} credits`
             break;
         
         default:
-            console.log ("Sorry, there is no delivery to your country")
+            message = "Sorry, there is no delivery to your country"
     }
+    return message;
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
